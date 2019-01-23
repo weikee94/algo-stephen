4e5a589bd1d39aa8ed3a43749495757c969adafb
wei kee haw <weikeehaw@weis-MacBook-Pro.local>
2019-01-23 17:24:49 +0800

    reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,42 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  // my approach
+  //   var splitStr = str.split("");
+  //   var revArr = splitStr.reverse();
+  //   console.log("Original: ", str);
+  //   console.log("Reverse str: ", revArr);
+
+  //   var emptyString = "";
+
+  //   for (i in revArr) {
+  //     emptyString = emptyString + revArr[i];
+  //   }
+
+  //   console.log("Result", emptyString);
+
+  //   return emptyString;
+
+  // first approach
+  //   return str
+  //     .split("")
+  //     .reverse()
+  //     .join("");
+
+  // second approach
+  // let reversed = "";
+
+  // for (let character of str) {
+  //   reversed = character + reversed;
+  // }
+
+  // return reversed;
+
+  // third approach
+  return str.split("").reduce((reversed, character) => {
+    return character + reversed;
+  }, "");
+}
 
 module.exports = reverse;
